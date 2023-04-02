a15235f52977d66bf8caf8e4079ad13371cf8812
Adam Griffin <adam.griffin@moxiworks.com>
2023-04-02 13:54:08 -0700

    Got Mongoose working

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,32 @@ import Koa from 'koa'
 import Router from '@koa/router'
 import bodyParser from 'koa-bodyparser'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
-const app = new Koa()
-const router = new Router()
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
+mongoose.connect(
+  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-router.get('/ping', async (ctx) => {
-  ctx.body = { message: 'pong' }
-})
+db.once('open', () => {
+  console.log('Connected to MongoDB')
 
-app.use(bodyParser())
-app.use(router.routes())
+  const app = new Koa()
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  const router = new Router()
+  router.get('/ping', async (ctx) => {
+    ctx.body = { message: 'pong' }
+  })
+
+  app.use(bodyParser())
+  app.use(router.routes())
+
+  const port = process.env.PORT || 3001
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
 })
