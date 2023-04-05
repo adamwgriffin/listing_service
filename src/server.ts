fcc881d72293bfbe2709201f3df3dbd960e82acd
Adam Griffin <adam.griffin@moxiworks.com>
2023-04-05 00:25:05 -0700

    Added database connection logic to server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
 import app from './app'
+import { connectToDatabase, disconnectDatabase } from './database'
 
-const port = process.env.APP_PORT || 3001
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+const startServer = async (): Promise<void> => {
+  try {
+    await connectToDatabase()
+    const port = process.env.APP_PORT || 3001
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port} 🚀`)
+    })
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+}
+
+const handleExit = async (): Promise<void> => {
+  console.log('Cleaning up before closing the server...')
+  await disconnectDatabase()
+  console.log('Cleanup complete. Closing the server...')
+  process.exit(0)
+}
+
+process.on('SIGTERM', handleExit)
+
+startServer()
