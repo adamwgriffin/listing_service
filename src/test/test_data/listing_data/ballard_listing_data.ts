60b601906f19b7a3468a78fdfbff3a7d72bdf924
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-04-22 20:20:45 -0700

    Added data for tests and dev

---
+++ src/test/test_data/listing_data/ballard_listing_data.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    "listPrice": 735000,
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -122.3779271,
+        47.6743378
+      ]
+    }
+  },
+  {
+    "listPrice": 1445995,
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -122.394911,
+        47.6797407
+      ]
+    }
+  }
+]
---
+++ src/test/test_data/listing_data/fremont_listing_data.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    "listPrice": 700000,
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -122.3507218,
+        47.6610594
+      ]
+    }
+  },
+  {
+    "listPrice": 809000,
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -122.3459572,
+        47.6495776
+      ]
+    }
+  },
+  {
+    "listPrice": 798000,
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        -122.3514925,
+        47.6629635
+      ]
+    }
+  }
+]
