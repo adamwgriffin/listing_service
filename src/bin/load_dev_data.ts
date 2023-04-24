504cfe9758c0317435807a78ea0356f24f13fbea
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-04-23 19:56:58 -0700

    Created Boundary model
    Along with associated data for test/dev

--- src/bin/load_dev_data.ts
+++ src/bin/load_dev_data.ts
@@ -3,6 +3,8 @@ import Listing from '../models/listingModel'
 import Boundary from '../models/BoundaryModel'
 import fremontListingData from '../test/test_data/listing_data/fremont_listing_data'
 import ballardListingData from '../test/test_data/listing_data/ballard_listing_data'
+import fremontBoundary from '../test/test_data/boundary_data/fremont_boundary'
+import ballardBoundary from '../test/test_data/boundary_data/ballard_boundary'
 
 const main = async (): Promise<void> => {
   try {
@@ -10,6 +12,9 @@ const main = async (): Promise<void> => {
     console.log("Creating listings...")
     const listings = await Listing.create(fremontListingData.concat(ballardListingData))
     console.log(`${listings.length} listings created.`)
+    console.log("Creating boundaries...")
+    const boundaries = await Boundary.create([fremontBoundary, ballardBoundary])
+    console.log(`${boundaries.length} Boundaries created.`)
     await disconnectDatabase()
     process.exit(0)
   } catch (err) {
---
+++ src/models/BoundaryModel.ts
@@ -0,0 +1,33 @@
+import type { IMultiPolygon } from './MultiPolygonSchema'
+import { Schema, model } from 'mongoose'
+import MultiPolygonSchema from './MultiPolygonSchema'
+
+export interface IBoundary extends Document {
+  name: string
+  type: 'neighborhood' | 'city' | 'zipcode' | 'county' | 'state' | 'country'
+  geometry: IMultiPolygon
+}
+
+export const BoundarySchema = new Schema<IBoundary>({
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['neighborhood', 'city', 'zipcode', 'county', 'state', 'country'],
+    required: true
+  },
+  geometry: {
+    type: MultiPolygonSchema,
+    // NOTE: it's very important that this index gets defined here, rather than on the coordinates in the
+    // MultiPolygonSchema. putting them on the coordinates breaks things so that you can never creating a record
+    // successfully
+    index: '2dsphere',
+    required: true
+  }
+})
+
+const Boundary = model<IBoundary>('Boundary', BoundarySchema)
+
+export default Boundary
