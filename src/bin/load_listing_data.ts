3824713900968ddaf0b633b25f9cafbdee549dfb
Adam Griffin <1782209+adamwgriffin@users.noreply.github.com>
2023-05-19 22:54:02 -0700

    Added command line args for load_listing_data

--- src/bin/load_listing_data.ts
+++ src/bin/load_listing_data.ts
@@ -1,22 +1,41 @@
 import type { IListing } from '../models/listingModel'
 import fs from 'fs'
 import path from 'path'
+import yargs from 'yargs'
 import { connectToDatabase, disconnectDatabase } from '../database'
 import Listing from '../models/listingModel'
 
+const DefaultFilePath = path.join(
+  __dirname,
+  '..',
+  'test',
+  'test_data',
+  'listing_data',
+  'random_listing_data.json'
+)
+
 const main = async (): Promise<void> => {
+  const argv = await yargs(process.argv.slice(2))
+    .option('file', {
+      type: 'string',
+      alias: 'f',
+      default: DefaultFilePath,
+      describe: 'Path to the file to use to load listing data'
+    })
+    .alias('h', 'help')
+    .help('help')
+    .usage(`Usage: $0 [options]`)
+    .argv
+
+  if (argv.help) {
+    yargs.showHelp()
+    process.exit(0)
+  }
+  
   try {
     await connectToDatabase()
     console.log("Creating listings...")
-    const filePath = path.join(
-      __dirname,
-      '..',
-      'test',
-      'test_data',
-      'listing_data',
-      'random_listing_data.json'
-    )
-    const listingData = JSON.parse(fs.readFileSync(filePath, 'utf-8')) as IListing[]
+    const listingData = JSON.parse(fs.readFileSync(argv.file, 'utf-8')) as IListing[]
     const listings = await Listing.create(listingData)
     console.log(`${listings.length} listings created.`)
     await disconnectDatabase()
