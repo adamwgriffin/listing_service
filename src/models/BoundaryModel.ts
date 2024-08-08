d18360e371774eb869344fa730bfcdd117472c8b
Adam Griffin <ag@agriffin.me>
2024-08-08 13:47:40 -0700

    Refactored BoundaryType
    We want to be able to use this constant outside of the module if necessary. We also create the types for it based on the values in the array instead of having to keep track of the values in both.

--- src/models/BoundaryModel.ts
+++ src/models/BoundaryModel.ts
@@ -2,15 +2,18 @@ import type { MultiPolygon } from '@turf/turf'
 import { Schema, model } from 'mongoose'
 import MultiPolygonSchema from './MultiPolygonSchema'
 
-export type BoundaryType =
-  | 'neighborhood'
-  | 'city'
-  | 'zip_code'
-  | 'county'
-  | 'state'
-  | 'country'
-  | 'school_district'
-  | 'school'
+export const BoundaryTypes = [
+  'neighborhood',
+  'city',
+  'zip_code',
+  'county',
+  'state',
+  'country',
+  'school_district',
+  'school'
+] as const
+
+export type BoundaryType = (typeof BoundaryTypes)[number]
 
 export interface IBoundary {
   name: string
@@ -27,22 +30,13 @@ export const BoundarySchema = new Schema<IBoundaryDocument>({
   },
   type: {
     type: String,
-    enum: [
-      'neighborhood',
-      'city',
-      'zip_code',
-      'county',
-      'state',
-      'country',
-      'school_district',
-      'school'
-    ],
+    enum: BoundaryTypes,
     required: true
   },
   geometry: {
     type: MultiPolygonSchema,
     // NOTE: it's very important that this index gets defined here, rather than on the coordinates in the
-    // MultiPolygonSchema. putting them on the coordinates breaks things so that you can never creating a record
+    // MultiPolygonSchema. putting them on the coordinates breaks things so that you can never create a record
     // successfully
     index: '2dsphere',
     required: true
