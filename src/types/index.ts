0984b711f24f0692c6d826b69011dfa0f1b0e920
Adam Griffin <ag@agriffin.me>
2024-09-29 17:56:02 -0700

    Created ControllerContext type

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+import { Context } from 'koa'
+
+/**
+ * Set the types for context request query/params as well as the response body
+ */
+export type ControllerContext<ContextType, ResponseBodyType> = ContextType & {
+  body: ResponseBodyType
+} & Context
