bce50b8f1bee3bc152aad51f5aba8008fc6132f5
Adam Griffin <ag@agriffin.me>
2025-01-24 18:26:29 -0800

    Fixed validation for /geocode endpoint

--- src/routes/geocodeRouter.ts
+++ src/routes/geocodeRouter.ts
@@ -1,10 +1,10 @@
 import Router from '@koa/router'
 import { geocodeRequest } from '../controllers/geocoderController'
 import { parseAndValidateRequest } from '../middlewares/validationMiddleware'
-import { geocodeRequestQuerySchema } from '../zod_schemas/geocodeRequestSchema'
+import { geocodeRequestSchema } from '../zod_schemas/geocodeRequestSchema'
 
 export default new Router().get(
   '/',
-  parseAndValidateRequest(geocodeRequestQuerySchema),
+  parseAndValidateRequest(geocodeRequestSchema),
   geocodeRequest
 )
