eccd274a635a84d8305bdc0b140386e6ab8aab4a
Adam Griffin <ag@agriffin.me>
2025-05-08 16:24:45 -0700

    Fixed broken mongo config
    It seems that this was broken when we added the "env" module for parsing environment variables. The test setup still needs proces.env instead of using the module.

--- src/test/globalSetup.ts
+++ src/test/globalSetup.ts
@@ -1,6 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
-import env from '../lib/env'
 import config from '../config/test.config'
 
 declare global {
@@ -9,18 +8,18 @@ declare global {
 }
 
 export default async function globalSetup() {
-  // Config to decided if an mongodb-memory-server instance should be used
   if (config.Memory) {
-    // it's needed in global space, because we don't want to create a new instance every test-suite
+    // Making it global because we don't want to create a new instance for every
+    // test-suite
     const instance = await MongoMemoryServer.create()
     const uri = instance.getUri()
     globalThis.__MONGOINSTANCE = instance
-    env.MONGO_URI = uri.slice(0, uri.lastIndexOf('/'))
+    process.env.MONGO_URI = uri.slice(0, uri.lastIndexOf('/'))
   } else {
-    env.MONGO_URI = `mongodb://${config.IP}:${config.Port}`
+    process.env.MONGO_URI = `mongodb://${config.IP}:${config.Port}`
   }
   // The following is to make sure the database is clean before any test starts
-  await mongoose.connect(`${env.MONGO_URI}/${config.Database}`)
+  await mongoose.connect(`${process.env.MONGO_URI}/${config.Database}`)
   await mongoose.connection.db.dropDatabase()
   await mongoose.disconnect()
 }
--- src/test/globalTeardown.ts
+++ src/test/globalTeardown.ts
@@ -1,10 +1,8 @@
-import { MongoMemoryServer } from 'mongodb-memory-server'
 import config from '../config/test.config'
 
 export default async function globalTeardown() {
-  // Config to decided if an mongodb-memory-server instance should be used
   if (config.Memory) {
-    const instance: MongoMemoryServer = globalThis.__MONGOINSTANCE
+    const instance = globalThis.__MONGOINSTANCE
     await instance.stop()
   }
 }
