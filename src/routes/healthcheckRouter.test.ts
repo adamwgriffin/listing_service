4a14a89d60ad389d7049914a0c45f1a06f36555d
Adam Griffin <ag@agriffin.me>
2025-05-10 12:35:22 -0700

    Fixed test
    The api was changed

--- src/routes/healthcheckRouter.test.ts
+++ src/routes/healthcheckRouter.test.ts
@@ -6,7 +6,7 @@ describe('healthcheckController', () => {
     it('should return status 200 and message "pong"', async () => {
       const response = await request(app.callback()).get('/ping')
       expect(response.status).toBe(200)
-      expect(response.body).toEqual({ message: 'pong' })
+      expect(response.text).toEqual('pong')
     })
   })
 })
