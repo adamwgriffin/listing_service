3d5d9514e1bebd123d34f8fc64b6440a619e0a3a
Adam Griffin <ag@agriffin.me>
2025-05-10 18:09:45 -0700

    Formatting

--- src/test/routes/listingSearchRouter.test.ts
+++ src/test/routes/listingSearchRouter.test.ts
@@ -6,18 +6,24 @@ describe('listingSearchRouter', () => {
   describe('GET /listing/search/boundary/:id', () => {
     it('returns a successful status when a boundary with the given ID exists', async () => {
       const boundary = await Boundary.findOne().lean()
-      const res = await request(app.callback()).get(`/listing/search/boundary/${boundary?._id}`)
+      const res = await request(app.callback()).get(
+        `/listing/search/boundary/${boundary?._id}`
+      )
       expect(res.status).toBe(200)
     })
 
     it('validates the boundary ID params type', async () => {
-      const res = await request(app.callback()).get(`/listing/search/boundary/bad_id_type`)
+      const res = await request(app.callback()).get(
+        `/listing/search/boundary/bad_id_type`
+      )
       expect(res.status).toBe(400)
     })
 
     it('returns a not found status when a boundary with the given ID does not exist', async () => {
       const nonExistentId = new Boundary()._id.toString()
-      const res = await request(app.callback()).get(`/listing/search/boundary/${nonExistentId}`)
+      const res = await request(app.callback()).get(
+        `/listing/search/boundary/${nonExistentId}`
+      )
       expect(res.status).toBe(404)
     })
   })
