5354c5c0580dc74d10984a97b78b4a119042e946
Adam Griffin <ag@agriffin.me>
2025-05-13 16:26:10 -0700

    Fixed type name

--- src/controllers/listingController.ts
+++ src/controllers/listingController.ts
@@ -3,12 +3,12 @@ import type { ListingDetailResponse } from '../types/listing_search_response_typ
 import type { ListingDetailRequest } from '../zod_schemas/listingDetailRequestSchema'
 import listingDetailView from '../views/listingDetailView'
 
-export type GeocodeBoundaryContext = ControllerContext<
+export type GetListingDetailContext = ControllerContext<
   ListingDetailRequest,
   ListingDetailResponse
 >
 
-export const getListingDetail = async (ctx: GeocodeBoundaryContext) => {
+export const getListingDetail = async (ctx: GetListingDetailContext) => {
   const { id } = ctx.params
   const listing = await ctx.repositories.listing.findByListingId(id)
   ctx.assert(listing, 404, `Listing not found with ID ${id}`)
