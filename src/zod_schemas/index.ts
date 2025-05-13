0372bce9ae2cc356519e915ad56aba8fb1339052
Adam Griffin <ag@agriffin.me>
2025-05-13 16:26:40 -0700

    Changed validation message

--- src/zod_schemas/index.ts
+++ src/zod_schemas/index.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const objectId = z.object({
-  id: z.string().regex(/^[0-9a-f]{24}$/, 'ID should be a MongoDB ObjectId')
+  id: z.string().regex(ValidObjectIdRegex, 'ID should be a valid ObjectId')
 })
 
 export const booleanEnum = z
