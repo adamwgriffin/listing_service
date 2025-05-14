ad6039a72975be9698c2d1d6ec64fba72a3ccf61
Adam Griffin <ag@agriffin.me>
2025-05-13 20:50:13 -0700

    Removed semi rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,5 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/semi": "off"
-  }
+  rules: {}
 };
