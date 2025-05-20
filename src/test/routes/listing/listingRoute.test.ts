207555dff24daaae9ae2c61d0574bfb1c07dc79a
Adam Griffin <ag@agriffin.me>
2025-05-19 18:44:25 -0700

    Refactored listing route

--- src/test/routes/listing/listingRoute.test.ts
+++ src/test/routes/listing/listingRoute.test.ts
@@ -1,20 +1,28 @@
 import request from "supertest";
 import { buildApp } from "../../../app";
-import {
-  getNonExistingListingId,
-  getRandomListingIds
-} from "../../testHelpers";
-
-const app = buildApp();
+import ListingModel from "../../../models/ListingModel";
+import listingTemplate from "../../data/listingTemplate";
+import { getNonExistingListingId } from "../../testHelpers";
 
 describe("GET /listing/:id", () => {
+  const app = buildApp();
+
+  let listingId: string;
+
+  beforeAll(async () => {
+    listingId = (await ListingModel.create(listingTemplate))._id.toString();
+  });
+
+  afterAll(async () => {
+    return ListingModel.deleteOne({ _id: listingId });
+  });
+
   it("validates the listing ID param type", async () => {
     const res = await request(app.callback()).get("/listing/invalid_type");
     expect(res.status).toBe(400);
   });
 
   it("returns the listing with the given ID", async () => {
-    const listingId = (await getRandomListingIds(1))[0];
     const res = await request(app.callback()).get(`/listing/${listingId}`);
     expect(res.status).toBe(200);
     expect(res.body._id).toBe(listingId);
