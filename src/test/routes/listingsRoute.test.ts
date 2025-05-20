3637d9718ab1ce3e7c59b86c648ebb73b892af3a
Adam Griffin <ag@agriffin.me>
2025-05-19 18:44:55 -0700

    Refactored listingsRoute

--- src/test/routes/listingsRoute.test.ts
+++ src/test/routes/listingsRoute.test.ts
@@ -1,20 +1,35 @@
 import request from "supertest";
 import { buildApp } from "../../app";
 import type { ListingsResponse } from "../../types/listing_search_response_types";
-import { getNonExistingListingId, getRandomListingIds } from "../testHelpers";
-
-const app = buildApp();
+import { getNonExistingListingId } from "../testHelpers";
+import listingTemplate from "../data/listingTemplate";
+import ListingModel from "../../models/ListingModel";
 
 describe("GET /listings/:ids", () => {
+  const app = buildApp();
+
+  let listingIds: string[];
+
+  beforeAll(async () => {
+    const listings = await ListingModel.create([
+      listingTemplate,
+      listingTemplate
+    ]);
+    listingIds = listings.map((l) => l._id.toString());
+  });
+
+  afterAll(async () => {
+    return ListingModel.deleteMany({ _id: { $in: listingIds } });
+  });
+
   it("validates the listing IDs params type for all IDs in the request", async () => {
     const res = await request(app.callback()).get(
-      "/listing/682515e68bfc5a47dcde63d3,invalid_type"
+      `/listing/${listingIds.join(",")},invalid_type`
     );
     expect(res.status).toBe(400);
   });
 
   it("returns the listings with the given IDs", async () => {
-    const listingIds = await getRandomListingIds(2);
     const res = await request(app.callback()).get(
       `/listings/${listingIds.join(",")}`
     );
@@ -24,18 +39,17 @@ describe("GET /listings/:ids", () => {
     expect(responseIds).toEqual(listingIds);
   });
 
-  describe("when no all the ids in the request were found", () => {
+  describe("when not all the ids in the request were found", () => {
     it("returns only the listings that were found without any errors", async () => {
       const nonExistentId = getNonExistingListingId();
-      const existingListingIds = await getRandomListingIds(1);
-      const requestIds = [...existingListingIds, nonExistentId];
+      const requestIds = [...listingIds, nonExistentId];
       const res = await request(app.callback()).get(
         `/listings/${requestIds.join(",")}`
       );
       expect(res.status).toBe(200);
       const data: ListingsResponse = res.body;
       const responseIds = data.listings.map((l) => l._id.toString());
-      expect(responseIds).toEqual(existingListingIds);
+      expect(responseIds).toEqual(listingIds);
     });
   });
 });
--- src/test/testHelpers.ts
+++ src/test/testHelpers.ts
@@ -12,13 +12,6 @@ export const listingsInsideBoundary = (
   );
 };
 
-export const getRandomListingIds = async (count: number) => {
-  const listings = await ListingModel.find({}, { _id: 1 }).limit(count).lean();
-  if (listings.length !== count)
-    throw new Error("Not enough listings found in test database");
-  return listings.map((l) => l._id.toString());
-};
-
 /** Get a listing id that does not exist in the database */
 export const getNonExistingListingId = () => new ListingModel()._id.toString();
 
