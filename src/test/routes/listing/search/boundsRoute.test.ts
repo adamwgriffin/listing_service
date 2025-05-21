6c8079513411571696b77f82d5d0cb68515f6f94
Adam Griffin <ag@agriffin.me>
2025-05-20 19:17:15 -0700

    Refactored bounds route

--- src/test/routes/listing/search/boundsRoute.test.ts
+++ src/test/routes/listing/search/boundsRoute.test.ts
@@ -1,7 +1,11 @@
+import { booleanPointInPolygon } from "@turf/turf";
+import { HydratedDocument } from "mongoose";
 import request from "supertest";
 import { buildApp } from "../../../../app";
+import { randomPointsWithinPolygon } from "../../../../lib/random_data";
+import ListingModel, { IListing } from "../../../../models/ListingModel";
 import { boundsParamsToGeoJSONPolygon } from "../../../../services/listingSearchService";
-import { testFilters } from "../../../testFilters";
+import listingTemplate from "../../../data/listingTemplate";
 import { listingsInsideBoundary } from "../../../testHelpers";
 
 const FremontViewportBounds = {
@@ -11,25 +15,51 @@ const FremontViewportBounds = {
   bounds_west: -122.38144953497519
 };
 
+const app = buildApp();
+
+const FremontViewportBoundsPolygon = boundsParamsToGeoJSONPolygon(
+  FremontViewportBounds
+);
+
 describe("GET /listing/search/bounds", () => {
-  const app = buildApp();
+  describe("bounds params", () => {
+    let listing: HydratedDocument<IListing>;
 
-  it("validates that all bounds params are present", async () => {
-    const res = await request(app.callback())
-      .get(`/listing/search/bounds`)
-      .query({ bounds_north: 47.69011227856514 });
-    expect(res.status).toBe(400);
+    beforeAll(async () => {
+      const point = randomPointsWithinPolygon(
+        FremontViewportBoundsPolygon,
+        1
+      )[0];
+      listing = await ListingModel.create({
+        ...listingTemplate,
+        geometry: point
+      });
+    });
+
+    afterAll(async () => {
+      await ListingModel.deleteOne({ _id: listing._id });
+    });
+
+    it("validates that all bounds params are present", async () => {
+      const res = await request(app.callback())
+        .get("/listing/search/bounds")
+        .query({ bounds_north: 47.69011227856514 });
+      expect(res.status).toBe(400);
+    });
+
+    it("only returns listings that are inside the bounds", async () => {
+      expect(
+        booleanPointInPolygon(
+          listing.geometry.coordinates,
+          FremontViewportBoundsPolygon
+        )
+      ).toBe(true);
+      const res = await request(app.callback())
+        .get("/listing/search/bounds")
+        .query(FremontViewportBounds);
+      expect(
+        listingsInsideBoundary(FremontViewportBoundsPolygon, res.body.listings)
+      ).toBe(true);
+    });
   });
-
-  it("only returns listings that are inside the bounds", async () => {
-    const res = await request(app.callback())
-      .get("/listing/search/bounds")
-      .query(FremontViewportBounds);
-    const geoJSONPolygon = boundsParamsToGeoJSONPolygon(FremontViewportBounds);
-    expect(listingsInsideBoundary(geoJSONPolygon, res.body.listings)).toBe(
-      true
-    );
-  });
-
-  testFilters(app, "/listing/search/bounds", FremontViewportBounds);
 });
