b6efec6cc382fbafd911171601031b1f2a881c13
Adam Griffin <ag@agriffin.me>
2025-05-27 18:19:15 -0700

    Set no-unused-vars to warn
    It's annoying

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,12 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended
+  tseslint.configs.recommended,
+  {
+    rules: {
+      // Note: you must disable the base rule as it can report incorrect errors
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  }
 );
