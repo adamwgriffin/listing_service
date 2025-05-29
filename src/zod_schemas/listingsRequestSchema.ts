5b436980d9c1c88c6aa5d8e9a3abaef892e87c30
Adam Griffin <ag@agriffin.me>
2025-05-29 15:37:21 -0700

    Trim string in array rather than raw value

--- src/zod_schemas/listingsRequestSchema.ts
+++ src/zod_schemas/listingsRequestSchema.ts
@@ -4,8 +4,7 @@ import { ValidObjectIdRegex } from ".";
 export const listingsParams = z.object({
   ids: z
     .string()
-    .trim()
-    .transform((ids) => ids.split(","))
+    .transform((ids) => ids.split(",").map((s) => s.trim()))
     .refine((ids) => ids.every((id) => ValidObjectIdRegex.test(id)), {
       message: "Every ID should be a valid ObjectId"
     })
