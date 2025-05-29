e31da175cb095f448483a82f0ac5a6f004e1525f
Adam Griffin <ag@agriffin.me>
2025-05-29 14:07:36 -0700

    Make Mongo connection log more clear

--- src/database.ts
+++ src/database.ts
@@ -9,7 +9,7 @@ mongoose.connection.on("error", (e) => {
 export const connectToDatabase = async (): Promise<void> => {
   try {
     const conn = await mongoose.connect(MongoDbUrl);
-    logger.debug(`MongoDB connected: ${conn.connection.host}`);
+    logger.debug(`MongoDB connected to host: ${conn.connection.host}`);
   } catch (err) {
     logger.error(err);
     process.exit(1);
