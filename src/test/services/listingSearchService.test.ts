b3c4fdbf5709afb96262d920271aed6fad66e4c6
Adam Griffin <ag@agriffin.me>
2025-06-22 17:26:08 -0700

    Added tests for getResultsForPlaceId()

--- src/test/services/listingSearchService.test.ts
+++ src/test/services/listingSearchService.test.ts
@@ -1,11 +1,39 @@
-import { ViewportBoundsExcludingFremontBoundary } from "../testHelpers";
-import fremontBoundary from "../data/fremontBoundary";
-import { getBoundaryGeometryWithBounds } from "../../services/listingSearchService";
 import { booleanPointInPolygon } from "@turf/turf";
-import { BoundsExcludingPartOfFremontBoundary } from "../testHelpers";
+import {
+  type PlaceIdLookupContext,
+  getBoundaryGeometryWithBounds,
+  getResultsForPlaceId
+} from "../../services/listingSearchService";
+import fremontBoundary from "../data/fremontBoundary";
+import {
+  BoundsExcludingPartOfFremontBoundary,
+  ViewportBoundsExcludingFremontBoundary
+} from "../testHelpers";
+import { z } from "zod";
+import { zBoundarySchema } from "../../zod_schemas/zBoundarySchema";
 
 const { insideBoundsPoint, outsideBoundsPoint, boundsParams, boundsPoly } =
   BoundsExcludingPartOfFremontBoundary;
+
+const mockPlaceIdLookupContext: PlaceIdLookupContext = {
+  query: {},
+  db: {
+    listing: {
+      findWithinBounds() {
+        return new Promise((resolve) => {
+          resolve([]);
+        });
+      }
+    },
+    boundary: {
+      findByPlaceId() {
+        return new Promise((resolve) => {
+          resolve(null);
+        });
+      }
+    }
+  }
+};
 
 describe("listingSearchService", () => {
   describe("getBoundaryGeometryWithBounds", () => {
@@ -62,6 +90,35 @@ describe("listingSearchService", () => {
           intersectionBoundary
         )
       ).toBe(true);
+    });
+  });
+
+  describe("getResultsForPlaceId", () => {
+    it("returns nothing if no boundary was found for the place_id", async () => {
+      const result = await getResultsForPlaceId(
+        "fake_place_id",
+        mockPlaceIdLookupContext
+      );
+      expect(result).toBeUndefined();
+    });
+
+    it("returns the boundary and the listing results if a boundary was found", async () => {
+      const ctx = { ...mockPlaceIdLookupContext };
+      ctx.db.boundary = {
+        findByPlaceId() {
+          return new Promise((resolve) => {
+            resolve(fremontBoundary);
+          });
+        }
+      };
+      const result = await getResultsForPlaceId(fremontBoundary.placeId, ctx);
+      const validResult = z
+        .object({
+          boundary: zBoundarySchema,
+          results: z.array(z.any())
+        })
+        .safeParse(result).success;
+      expect(validResult).toBe(true);
     });
   });
 });
